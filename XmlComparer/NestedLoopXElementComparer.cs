78faeba1471519f2e4fb174a15d690e72e40249b
Dan Kalotay <daniel.kalotay@7digital.com>
2014-01-31 18:10:06 +0000

    initial commit for xelement comparer

---
+++ XmlComparer/NestedLoopXElementComparer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlComparer
+{
+    public class NestedLoopXElementComparer : IEqualityComparer<XElement>
+    {
+        public bool Equals(XElement a, XElement b)
+        {
+            if (XNode.DeepEquals(a, b))
+            {
+                return true;
+            }
+
+            if (!a.HasElements)
+            {
+                return false;
+            }
+
+            if (!b.HasElements)
+            {
+                return false;
+            }
+
+            var aElements = a.Elements().ToList();
+            var bElements = a.Elements().ToList();
+
+            if (aElements.Count != bElements.Count)
+            {
+                return false;
+            }
+
+            var commonCount = 0;
+            foreach (var aElement in aElements)
+            {
+                foreach (var bElement in bElements)
+                {
+                    if (Equals(aElement, bElement))
+                    {
+                        commonCount += 1;
+                    }
+                }
+            }
+
+            return commonCount == aElements.Count;
+        }
+
+        public int GetHashCode(XElement obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ XmlComparer/TheTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace XmlComparer
+{
+    [TestFixture]
+    public class TheTests
+    {
+        private const string XmlA = @"<report><bodyCollection>
+<item currency=""EUR"" amount = ""1.0""/>
+<item currency=""GBP"" amount = ""2.0""/>
+</bodyCollection></report>";
+
+        private const string XmlB = @"<report><bodyCollection>
+<item currency=""GBP"" amount = ""2.0""/>
+<item currency=""EUR"" amount = ""1.0""/>
+</bodyCollection></report>";
+
+        [Test]
+        public void XNodeDeepEqualsWorksAsExpected()
+        {
+            var xmlA = XDocument.Parse(XmlA);
+            var xmlB = XDocument.Parse(XmlB);
+            Assert.That(XNode.DeepEquals(xmlA.Root, xmlB.Root), Is.False);
+        }
+
+        [Test]
+        public void NestedLoopDeepEqualsWorksAsExpected()
+        {
+            var xmlA = XDocument.Parse(XmlA);
+            var xmlB = XDocument.Parse(XmlB);
+            Assert.That(new NestedLoopXElementComparer().Equals(xmlA.Root, xmlB.Root), Is.True);
+        }
+
+        [Test]
+        public void SortMergeDeepEqualsWorksAsExpected()
+        {
+            var xmlA = XDocument.Parse(XmlA);
+            var xmlB = XDocument.Parse(XmlB);
+            Assert.That(new SortMergeXElementComparer().Equals(xmlA.Root, xmlB.Root), Is.True);
+        }
+
+        public static bool Equals(XElement a, XElement b)
+        {
+            return new NestedLoopXElementComparer().Equals(a, b);
+        }
+    }
+}
