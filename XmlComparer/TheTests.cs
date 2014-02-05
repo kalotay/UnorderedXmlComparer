e904d37d29abfdfe626e3fde7ab0c3c75fa18328
Dan Kalotay <daniel.kalotay@7digital.com>
2014-02-05 17:56:50 +0000

    introduce fuzzing

--- XmlComparer/TheTests.cs
+++ XmlComparer/TheTests.cs
@@ -27,6 +27,40 @@ namespace XmlComparer
 </bodyCollection></report>";
 
         [Test]
+        public void FuzzingCausesDeepEqualsToFail()
+        {
+            var xml = XDocument.Parse(XmlA);
+            var fuzzed = new XmlFuzzer().Fuzz(xml);
+            Assert.That(XNode.DeepEquals(xml.Root, fuzzed.Root), Is.False);
+        }
+
+        [Test]
+        public void FuzzingCausesNestedLoopEqualToFailDueToAttributeReordering()
+        {
+            var xml = XDocument.Parse(XmlA);
+            var fuzzed = new XmlFuzzer().Fuzz(xml);
+            var nestedLoopComparer = new NestedLoopXElementComparer();
+            Assert.That(nestedLoopComparer.Equals(xml.Root, fuzzed.Root), Is.False);
+        }
+
+        [Test]
+        public void FuzzingHasNoEffectOnUnorderedNodeParser()
+        {
+            var xml = XDocument.Parse(XmlA);
+            var fuzzed = new XmlFuzzer().Fuzz(xml);
+            var parser = new UnorderedNodeParser();
+            using (var reader = xml.CreateReader())
+            {
+                var source = parser.Parse(reader);
+                using (var fuzzedReader = fuzzed.CreateReader())
+                {
+                    var fuzz = parser.Parse(fuzzedReader);
+                    Assert.That(source, Is.EqualTo(fuzz));
+                }
+            }
+        }
+
+        [Test]
         public void XNodeDeepEqualsWorksAsExpected()
         {
             var xmlA = XDocument.Parse(XmlA);
---
+++ XmlComparer/XmlFuzzer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlComparer
+{
+    public class XmlFuzzer
+    {
+        public XDocument Fuzz(XDocument document)
+        {
+            var root = document.Root;
+            var fuzzedRoot = Fuzz(root);
+            var fuzzedDoc = new XDocument(fuzzedRoot);
+            return fuzzedDoc;
+        }
+
+        public XElement Fuzz(XElement element)
+        {
+            var name = element.Name;
+            var fuzzedElement = new XElement(name);
+
+            if (element.HasAttributes)
+            {
+                var shuffledAttributes = Shuffle(element.Attributes().ToList());
+                foreach (var attribute in shuffledAttributes)
+                {
+                    fuzzedElement.SetAttributeValue(attribute.Name, attribute.Value);
+                }
+            }
+
+            if (element.HasElements)
+            {
+                var shuffledElements = Shuffle(element.Elements().ToList());
+                foreach (var fuzzed in shuffledElements.Select(Fuzz))
+                {
+                    fuzzedElement.Add(fuzzed);
+                }
+            }
+            else
+            {
+                fuzzedElement.Value = element.Value;
+            }
+
+            return fuzzedElement;
+        }
+
+        public static IList<T> Shuffle<T>(IList<T> list)
+        {
+            var rng = new Random();
+            var shuffled = new List<T>(list);
+            for (var i = 1; i < list.Count; i++)
+            {
+                var randomInt = rng.Next(0, i + 1);
+                if (randomInt != i)
+                {
+                    shuffled[i] = shuffled[randomInt];
+                }
+                shuffled[randomInt] = list[i];
+            }
+            return shuffled;
+        }
+    }
+}
